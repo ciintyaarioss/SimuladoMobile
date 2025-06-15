e83e2afc3b6d6a93964a3e4f42dd03a5fda31c7f
unknown <breno.souza@germinare.org.br>
2025-06-15 11:37:44 -0300

    adicionando lógica da dialog

--- app/src/main/java/com/simuladomobile/simuladomobileJBS/ui/activities/MainActivity.java
+++ app/src/main/java/com/simuladomobile/simuladomobileJBS/ui/activities/MainActivity.java
@@ -1,47 +1,59 @@
 package com.simuladomobile.simuladomobileJBS.ui.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.simuladomobile.simuladomobileJBS.R;
 import com.simuladomobile.simuladomobileJBS.databinding.ActivityMainBinding;
+import com.simuladomobile.simuladomobileJBS.ui.fragments.PasswordDialogFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private RecyclerView recyclerView;
-
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        BottomNavigationView navView = binding.navView;
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_registrar_carro, R.id.navigation_administrador)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupWithNavController(binding.navView, navController);
 
-//        RegistroCarroRepository registroCarroRepository = new RegistroCarroRepository();
-//        registroCarroRepository.save(
-//                new RegistroCarro(LocalDate.now(), LocalDate.of(2025, 12, 30), "ABC1234")
-//        );
+        NavigationUI.setupWithNavController(navView, navController);
 
+        navView.setOnItemSelectedListener(item -> handleNavigation(item));
+    }
 
+    private boolean handleNavigation(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
 
+        if (itemId == R.id.navigation_registrar_carro) {
+            navController.navigate(R.id.navigation_registrar_carro);
+            return true;
+
+        } else if (itemId == R.id.navigation_administrador) {
+            PasswordDialogFragment dialog = PasswordDialogFragment.newInstance(() -> {
+                navController.navigate(R.id.navigation_administrador);
+            });
+            dialog.show(getSupportFragmentManager(), "PasswordDialog");
+            return false;
+        }
+
+        return false;
     }
 }
